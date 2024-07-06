c4d9fa215e849a92b81f5345d0903665a4a9d9e2
zebubull <ldkourtev@gmail.com>
2024-07-06 19:47:13 +0000

    add force update

--- update.sh
+++ update.sh
@@ -3,15 +3,19 @@ if [[ -f "/etc/pkglist.txt" && -f "./pkglist.txt" ]]; then
     cat /etc/pkglist.txt | grep --color=never -v -x -f pkgignore.txt > /tmp/current-packages.txt
     if cmp -s "./pkglist.txt" "/tmp/current-packages.txt"; then
         echo "Package list is up-to-date."
-    elif [[ "./pkglist.txt" -nt "/etc/pkglist.txt" ]]; then
-        echo "./pkglist.txt is newer, run sync.sh to sync your system."
+    elif [[ "./pkglist.txt" -nt "/etc/pkglist.txt" && $1 != "-f" ]]; then
+        echo "./pkglist.txt is newer, run ./sync.sh to sync your system or run ./update.sh -f to force an update."
     else
-        echo "/etc/pkglist.txt is newer, updating ./pkglist.txt."
+        if [[ $1 == "-f" ]]; then
+            echo "[WARNING]: Force updating ./pkglist.txt, unsynced packages will be removed."
+        else
+            echo "/etc/pkglist.txt is newer, updating ./pkglist.txt."
+        fi
         echo "Added packages:"
         git -P diff --no-index -U0 -- pkglist.txt /etc/pkglist.txt | grep --color=always '^[+-][^+-]\S*' | grep -v -f pkgignore.txt
         cat /etc/pkglist.txt | grep --color=never -v -x -f pkgignore.txt > pkglist.txt
         echo "Done."
     fi
 else
-    echo "Run sync.sh to bootstrap your system."
+    echo "Run ./sync.sh to bootstrap your system."
 fi
