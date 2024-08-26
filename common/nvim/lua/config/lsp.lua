1fe9815387613ce4760b323acad3ce8793464058
zebubull <ldkourtev@gmail.com>
2024-08-26 12:58:41 -0400

    laptop stuff I guess

--- common/nvim/lua/config/lsp.lua
+++ common/nvim/lua/config/lsp.lua
@@ -67,7 +67,6 @@ lspsaga.setup({
     },
 })
 
-
 vim.api.nvim_create_autocmd('LspAttach', {
     group = vim.api.nvim_create_augroup('UserLspConfig', {}),
     callback = lsp_on_attach,
---
+++ framework/kitty/tab_bar.py
@@ -0,0 +1,57 @@
+from kitty.fast_data_types import Screen, get_options
+from kitty.tab_bar import DrawData, ExtraData, TabBarData, draw_title, as_rgb
+from kitty.utils import color_as_int
+
+opts = get_options()
+
+fg = as_rgb(color_as_int(opts.color8))
+bg = as_rgb(color_as_int(opts.color7))
+active_fg = as_rgb(color_as_int(opts.color15))
+
+def draw_tab(
+    draw_data: DrawData, screen: Screen, tab: TabBarData,
+    before: int, max_title_length: int, index: int, is_last: bool,
+    extra_data: ExtraData
+) -> int:
+    def _do_round(text: str):
+        screen.cursor.bg = bg
+        screen.cursor.fg = fg
+        screen.draw(text)
+        screen.cursor.bg = fg
+        screen.cursor.fg = bg
+
+    def _do_title():
+        if tab.needs_attention:
+            screen.draw(f'!{tab.title}!')
+        else:
+            screen.draw(tab.title)
+
+    orig_fg = screen.cursor.fg
+    orig_bg = screen.cursor.bg
+    is_first = before == 0
+
+    screen.cursor.bg = fg
+    screen.cursor.fg = bg
+
+    if is_first:
+        screen.draw(' ')
+    else:
+        screen.draw('｜ ')
+
+    if tab.is_active:
+        screen.cursor.fg = active_fg
+        screen.cursor.bold = True
+        _do_title()
+        screen.cursor.bold = False
+    else:
+        _do_title()
+
+    if is_last:
+        screen.draw(' ' * (screen.columns - screen.cursor.x))
+    else:
+        screen.draw(f' ')
+
+    screen.cursor.bg = orig_bg
+    screen.cursor.fg = orig_fg
+
+    return screen.cursor.x
