c947e76e6e3f117d8ffc6293026d21abcb8e3a25
zebubull <ldkourtev@gmail.com>
2024-12-16 02:45:01 -0500

    all(fish): add hostname to prompt
    added user@host to fish prompt

--- common/fish/functions/fish_prompt.fish
+++ common/fish/functions/fish_prompt.fish
@@ -4,5 +4,5 @@ function fish_prompt -d "Write out the prompt"
     if test $last_status -ne 0
         set stat (set_color red)"[$last_status]"(set_color normal)
     end
-    string join '' -- (set_color -o green) (prompt_pwd) (set_color normal) $stat (fish_git_prompt) (set_color cyan)'> ' (set_color normal)
+    string join '' -- (set_color -o brblue) $USER "@" $hostname " "(set_color -o green) (prompt_pwd) (set_color normal) $stat (fish_git_prompt) (set_color cyan)'> ' (set_color normal)
 end
