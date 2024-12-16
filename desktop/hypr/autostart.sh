5e5c8c9da7c192fc0ce7c8d9f1322876b3781368
zebubull <ldkourtev@gmail.com>
2024-12-16 02:08:05 -0500

    desktop(hypr): fix discord autostart
    made autostart script start discord-canary instead of discord

--- desktop/hypr/autostart.sh
+++ desktop/hypr/autostart.sh
@@ -5,7 +5,7 @@ hyprctl dispatch workspace 2
 firefox >/dev/null 2>/dev/null &
 sleep 3
 hyprctl dispatch workspace 3
-discord >/dev/null 2>/dev/null &
+discord-canary >/dev/null 2>/dev/null &
 disown
 sleep 8
 hyprctl dispatch workspace 4
