7a1b412866e333bbcfcdbad71073b6f3b402cd39
zebubull <ldkourtev@gmail.com>
2024-12-16 02:05:38 -0500

    archbtw: delete old config
    removed old config for unused machine 'archbtw'

--- archbtw/eww/launch_bar.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-eww active-windows | grep -q bar
-status=$?
-if [[ $status == 1 ]]; then
-    eww open bar
-else
-    eww close bar
-fi
--- archbtw/eww/launch_dash.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-eww active-windows | grep -q dash
-status=$?
-if [[ $status == 1 ]]; then
-    eww open dash
-else
-    eww close dash
-fi
